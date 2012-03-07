5487d1d2f37ec0e3162f16da9bc157df8e76ae2e
Emil Grunt <emilgrunt@gmail.com>
2012-03-07 15:41:15 +0100

    added test project

---
+++ Test Prosjekt/WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
@@ -0,0 +1,217 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Kinect;
+
+namespace WindowsGame1
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D kinectRGBVideo;
+
+        KinectSensor kinectSensor;
+        SpriteFont font;
+
+        string connectedStatus = "Not connected";
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 640;
+            graphics.PreferredBackBufferHeight = 480;
+        }
+
+        void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            if (kinectSensor == e.Sensor)
+            {
+                if (e.Status == KinectStatus.Disconnected || e.Status == KinectStatus.NotPowered)
+                {
+                    kinectSensor = null;
+                    DiscoverKinectSensor();
+                }
+            }
+        }
+
+        private bool InitializeKinect()
+        {
+            kinectSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            kinectSensor.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(kinectSensor_ColorFrameReady);
+
+            try
+            {
+                kinectSensor.Start();
+            }
+            catch
+            {
+                connectedStatus = "Unable to start the Kinect Sensor";
+                return false;
+            }
+            return true;
+        }
+
+        void kinectSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorImageFrame = e.OpenColorImageFrame())
+            {
+                if (colorImageFrame != null)
+                {
+                    byte[] pixelsFromFrame = new byte[colorImageFrame.PixelDataLength];
+
+                    colorImageFrame.CopyPixelDataTo(pixelsFromFrame);
+
+                    Color[] color = new Color[colorImageFrame.Height * colorImageFrame.Width];
+                    kinectRGBVideo = new Texture2D(graphics.GraphicsDevice, colorImageFrame.Width, colorImageFrame.Height);
+
+                    int index = 0;
+                    for (int y = 0; y < colorImageFrame.Height; ++y)
+                    {
+                        for (int x = 0; x < colorImageFrame.Width; ++x, index += 4)
+                        {
+                            color[y * colorImageFrame.Width + x] = new Color(pixelsFromFrame[index + 2],
+                                        pixelsFromFrame[index + 1], pixelsFromFrame[index + 0]);
+                        }
+                    }
+
+                    kinectRGBVideo.SetData(color);
+                }
+            }
+        }
+
+        private void DiscoverKinectSensor()
+        {
+            foreach (KinectSensor sensor in KinectSensor.KinectSensors)
+            {
+                if (sensor.Status == KinectStatus.Connected)
+                {
+                    kinectSensor = sensor;
+                    break;
+                }
+            }
+
+            if (kinectSensor == null)
+            {
+                connectedStatus = "Found none Kinect Sensors connect to USB";
+                return;
+            }
+
+            switch (kinectSensor.Status)
+            {
+                case KinectStatus.Connected:
+                    {
+                        connectedStatus = "Status: Connected";
+                        break;
+                    }
+                case KinectStatus.Disconnected:
+                    {
+                        connectedStatus = "Status: Disconnected";
+                        break;
+                    }
+                case KinectStatus.NotPowered:
+                    {
+                        connectedStatus = "Status: Connect the power";
+                        break;
+                    }
+                default:
+                    {
+                        connectedStatus = "Status: Error";
+                        break;
+                    }
+            }
+
+            if (kinectSensor.Status == KinectStatus.Connected)
+            {
+                InitializeKinect();
+            }
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            KinectSensor.KinectSensors.StatusChanged += new EventHandler<StatusChangedEventArgs>(KinectSensors_StatusChanged);
+            DiscoverKinectSensor();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            kinectRGBVideo = new Texture2D(GraphicsDevice, 1337, 1337);
+
+            font = Content.Load<SpriteFont>("SpriteFont1");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            kinectSensor.Stop();
+            kinectSensor.Dispose();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(kinectRGBVideo, new Rectangle(0, 0, 640, 480), Color.White);
+            spriteBatch.DrawString(font, connectedStatus, new Vector2(20, 80), Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
