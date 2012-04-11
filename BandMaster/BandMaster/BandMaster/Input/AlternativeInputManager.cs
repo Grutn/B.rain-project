2a6615c55e3e39e0cc5b29fd6f1bce294aea4597
Emil Grunt <emilgrunt@gmail.com>
2012-04-11 15:43:37 +0200

    split input classes to individual files and added empty events for keyboard input

---
+++ BandMaster/BandMaster/BandMaster/Input/AlternativeInputManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace BandMaster.Input
+{
+    public class AlternativeInputManager : GameComponent, IManageInput
+    {
+        private KeyboardState currState;
+        private KeyboardState lastState;
+
+        public event EventHandler<PlayerEvent> OnPlayerEvent; // on kinect: all movements of hands; on keyboard: mouse movements
+        public event EventHandler OnTempoHit; // on kinect: detect in kinectinput subclass; on keyboard: some key event
+        public event EventHandler OnDynamicHit; // same as above
+
+        public AlternativeInputManager(Game game) : base(game)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            lastState = currState;
+            currState = Keyboard.GetState();
+
+            if (IsNewKeyPress(Keys.J))
+            {
+                OnTempoHit(this, null);
+            }
+            if (IsNewKeyPress(Keys.F))
+            {
+                OnDynamicHit(this, new PlayerEvent());
+            }
+            
+        }
+
+        private bool IsNewKeyPress(Keys key)
+        {
+            return currState.IsKeyDown(key) && lastState.IsKeyDown(key);
+        }
+    }
+}
